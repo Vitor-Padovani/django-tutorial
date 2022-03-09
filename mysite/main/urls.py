514a7435122fba3dd46224c4b3106afbbf896f13
Vitor Azevedo Padovani <98499539+Vitor-Padovani@users.noreply.github.com>
2022-03-09 13:51:02 -0300

    set_up

---
+++ mysite/main/urls.py
@@ -0,0 +1,10 @@
+# CREATED
+
+from django.urls import path
+from . import views
+
+# secondary urls
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("v1", views.v1, name="view 1"),
+]
