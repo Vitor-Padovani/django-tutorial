ddf9a342530dc57036f1d4eb53bb27039ec78be2
Vitor Azevedo Padovani <98499539+Vitor-Padovani@users.noreply.github.com>
2022-03-11 08:54:29 -0300

    add_item

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -6,6 +6,26 @@ from .forms import CreateNewList
 # Create your views here.
 def index(response, num):
     ls = ToDoList.objects.get(id=num)
+    if response.method == "POST":
+        print(response.POST)
+        if response.POST.get("save"): # if .get doesn't find: return None
+
+            for item in ls.item_set.all():
+                if response.POST.get("c" + str(item.id)) == "clicked":
+                    item.complete = True
+                else:
+                    item.complete = False
+
+                item.save()
+
+        elif response.POST.get("newItem"):
+            txt = response.POST.get("new")
+
+            if len(txt) > 2:
+                ls.item_set.create(text=txt, complete=False)
+            else:
+                print("invalid")
+
     return render(response, "main/list.html", {"ls": ls})
 
 def home(response):
